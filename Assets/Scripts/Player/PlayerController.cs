1eabd037e2def015d46daaf31e6fa59339169149
Caique Portela <caiquesportela@gmail.com>
2021-08-30 22:15:20 -0300

    Implementando o desenho do level e ajustando colisão com o level

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using Helper;
 using UnityEngine;
 
@@ -17,10 +16,16 @@ namespace Player
 
         private int _jumps;
 
+        private BoxCollider2D _boxCollider2D;
+
+        [SerializeField] private LayerMask layerLevel;
+        private float _distanceFloorCollision = 0.5f;
+
         void Start()
         {
             this._rigidbody2D = GetComponent<Rigidbody2D>();
             this._animator = GetComponentInChildren<Animator>();
+            this._boxCollider2D = GetComponent<BoxCollider2D>();
         }
 
         void Update()
@@ -28,6 +33,23 @@ namespace Player
             this.Move();
 
             this.Jump();
+        }
+
+        private void FixedUpdate()
+        {
+            OnFloor();
+        }
+
+        private void OnFloor()
+        {
+            var isGrounded = this.IsGrounded();
+
+            this._animator.SetBool(AnimationParameter.InFloor, isGrounded);
+
+            if (isGrounded)
+            {
+                this._jumps = 0;
+            }
         }
 
         private void Move()
@@ -47,26 +69,41 @@ namespace Player
 
         private void Jump()
         {
+            Debug.Log($"{this._jumps} {this.limitJumps}");
             if (Input.GetButtonDown("Jump") && this._jumps < this.limitJumps)
             {
                 var newVelocity = new Vector2(this._rigidbody2D.velocity.x, this.velocityJump);
 
                 this._rigidbody2D.velocity = newVelocity;
-                
-                this._animator.SetBool(AnimationParameter.InFloor, false);
+
+                // this._animator.SetBool(AnimationParameter.InFloor, false);
 
                 this._jumps++;
             }
-            
+
             this._animator.SetFloat(AnimationParameter.AirPosition, this._rigidbody2D.velocity.y);
+        }
+
+        private bool IsGrounded()
+        {
+            // Checando colisão com o chão
+            bool inFloor = Physics2D.Raycast(this._boxCollider2D.bounds.center, Vector2.down,
+                this._distanceFloorCollision, this.layerLevel);
+
+            var color = inFloor ? Color.red : Color.green;
+
+            // Desenhando linha com o chão
+            Debug.DrawRay(this._boxCollider2D.bounds.center, Vector3.down * this._distanceFloorCollision, color);
+
+            return inFloor;
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject.CompareTag(Tags.Floor))
             {
-                this._jumps = 0;
-                this._animator.SetBool(AnimationParameter.InFloor, true);
+                // this._jumps = 0;
+                // this._animator.SetBool(AnimationParameter.InFloor, true);
             }
         }
 
@@ -74,7 +111,7 @@ namespace Player
         {
             if (other.gameObject.CompareTag(Tags.Floor))
             {
-                this._animator.SetBool(AnimationParameter.InFloor, false);
+                // this._animator.SetBool(AnimationParameter.InFloor, false);
             }
         }
     }
