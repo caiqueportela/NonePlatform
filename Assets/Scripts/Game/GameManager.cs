97564eb594fdca0e1a41581fd36114387c2309d4
Caique Portela <caiquesportela@gmail.com>
2021-09-02 23:08:49 -0300

    - Exibindo vida do jogador - Construindo plataformas

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Game
 {
@@ -8,15 +9,25 @@ namespace Game
     {
         
         private static int _playerLife = 3;
+
+        [SerializeField] private Image[] hearths;
         
         void Start()
         {
-        
+            this.UpdateHearths();
         }
         
         void Update()
         {
             
+        }
+
+        private void UpdateHearths()
+        {
+            for (int i = 0; i < this.hearths.Length; i++)
+            {
+                this.hearths[i].enabled = GameManager._playerLife >= (i + 1);
+            }
         }
 
         public void ResetGame()
@@ -28,6 +39,7 @@ namespace Game
         public void TakeDamage(int damage)
         {
             GameManager._playerLife -= damage;
+            this.UpdateHearths();
         }
 
         public int GetPlayerLife()
